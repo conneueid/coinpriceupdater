ff5cb875cc045cc3dd547fd8ffd344328fdfbfee
Conner S Quintero <95557031+conneueid@users.noreply.github.com>
2022-10-25 20:20:57 -0500

    Add simple way to kill process

--- bitcoinupdater.py
+++ bitcoinupdater.py
@@ -1,6 +1,7 @@
 import cryptocompare
 import time
-
+import os
+import sys
 ##cryptocompare.cryptocompare._set_api_key_parameter(6e8cebd645566fda2804cffb4490a4ca21d63045c06784b8f48b243966fc0b39)
 
 
@@ -10,16 +11,24 @@ option = input("Enter Coin: ")
 
 
 if option == "BTC":
+	print("Type Exit To Stop :)")
 	while True:
 		print(cryptocompare.get_price('BTC', currency='USD', full=False))
+		exit = input()
+		if exit == "Exit":
+			sys.exit("Exiting")
 		##exit = input("Type Exit To Stop: ")
 		##if exit == "Exit":
 			##os.execl(sys.executable, sys.executable, *sys.argv)
 		time.sleep(5)
 
 if option == "ETH":
+	print("Type Exit To Stop :)")
 	while True:
 		print(cryptocompare.get_price('ETH', currency='USD', full=False))
+		exit = input()
+		if exit == "Exit":
+			sys.exit("Exiting")
 		##exit = input("Type Exit To Stop: ")
 		##if exit == "Exit":
 			##os.execl(sys.executable, sys.executable, *sys.argv)
