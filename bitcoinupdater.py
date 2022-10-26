bc4de0d51b5f841f99c5097ef2802d557deaeb51
conneueid <95557031+conneueid@users.noreply.github.com>
2022-10-25 20:00:12 -0500

    Introduce Stopping While Loop For Future

--- bitcoinupdater.py
+++ bitcoinupdater.py
@@ -1,5 +1,27 @@
 import cryptocompare
 import time
-while True:
-	print(cryptocompare.get_price('BTC', currency='USD', full=False))
-	time.sleep(5)
+
+##cryptocompare.cryptocompare._set_api_key_parameter(6e8cebd645566fda2804cffb4490a4ca21d63045c06784b8f48b243966fc0b39)
+
+
+print("Choose Coin To See Live Price Of :)")
+print("Available Coins, BTC, ETH")
+option = input("Enter Coin: ")
+
+
+if option == "BTC":
+	while True:
+		print(cryptocompare.get_price('BTC', currency='USD', full=False))
+		##exit = input("Type Exit To Stop: ")
+		##if exit == "Exit":
+			##os.execl(sys.executable, sys.executable, *sys.argv)
+		time.sleep(5)
+
+if option == "ETH":
+	while True:
+		print(cryptocompare.get_price('ETH', currency='USD', full=False))
+		##exit = input("Type Exit To Stop: ")
+		##if exit == "Exit":
+			##os.execl(sys.executable, sys.executable, *sys.argv)
+		time.sleep(5)
+
